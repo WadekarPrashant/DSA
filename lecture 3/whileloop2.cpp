c4326bfd2ef0491156fe229519bd51cc0fb14c82
WadekarPrashant <wadekarprashant733@gmail.com>
2022-09-10 14:15:35 +0530

    whileLoop

---
+++ lecture 3/whileloop2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int i=1;
+
+    while(i<=10)
+    {
+        int a = i*6;
+        cout<<a<<endl;
+        i++;
+
+    }
+
+return 0;
+}
