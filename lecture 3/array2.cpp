311ab97b34f335b4f8f15744a77273925819d584
WadekarPrashant <wadekarprashant733@gmail.com>
2022-09-11 23:53:09 +0530

    modified

--- lecture 3/array2.cpp
+++ lecture 3/array2.cpp
@@ -6,10 +6,11 @@ int main()
    int sum = 0;   
   int arr[] = {45, 88, 62, 99, 11, 22, 33};
 
-    for( int j=0 ; j<= 7 ; j++){
+    for( int j=0 ; j< 7 ; j++){
         
         cout<<"The value of index"<<" "<<j<<" is "<<arr[j]<<endl;
-        sum = arr[j];
+
+        sum = sum + arr[j];
     }
     cout<<"The sum of array is "<<sum;
 
