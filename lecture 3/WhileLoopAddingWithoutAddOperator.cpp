53d072bc1a68c6f949f1ae64f480fe8bff9909fe
WadekarPrashant <wadekarprashant733@gmail.com>
2022-09-21 21:41:23 +0530

    WhileLoop

---
+++ lecture 3/WhileLoopAddingWithoutAddOperator.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int Number1,Number2;
+    cout<<"Please Enter the First number:";
+    cin>>Number1;
+    cout<<"Please Enter the second number:";
+    cin>>Number2;
+ while (Number2 != 0)
+    {
+        int carrynNumber = Number1 & Number2;
+        Number1 = Number1 ^ Number2;
+        Number2 = carrynNumber << 1;
+    }
+    cout<<"Thu sum of two numbers is: "<<Number1;
+}
