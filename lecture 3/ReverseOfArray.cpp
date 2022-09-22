eedaba531aa1519d2034f225817c2d32c463c425
WadekarPrashant <wadekarprashant733@gmail.com>
2022-09-22 17:09:34 +0530

    reverse

---
+++ lecture 3/ReverseOfArray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ 
+   
+    int arr[] = {90, 46, 68, 13, 2, 23};
+    int tc;
+    for(tc=5; tc>=0; tc--){
+
+    cout<<arr[tc]<<" ";
+   }
+   
+    return 0;
+} 
