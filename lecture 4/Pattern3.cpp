788373fb04911075bcd1da7dafdaed70c40953f2
WadekarPrashant <wadekarprashant733@gmail.com>
2022-10-02 13:17:17 +0530

    pattern4

---
+++ lecture 4/Pattern3.cpp
@@ -0,0 +1,34 @@
+
+// Printing numbers
+// 1111
+// 2222
+// 3333
+// 4444
+
+// if the number (n) of rows and columns are same 
+// i is the  rows 
+// j is the  columns
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the no. of rows or columns";
+    cin>>n;
+
+    int i=1;
+    while(i<=n){ 
+        int j=0;
+        while(j<n){
+            
+            cout<<i;
+            j++;
+        }
+            cout<<endl;
+        i++;
+    }
+
+    return 0;
+} 
