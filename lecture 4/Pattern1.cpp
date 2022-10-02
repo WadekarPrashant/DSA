377f6ca03a37464585af1394d5b6bc4e0d99efce
WadekarPrashant <wadekarprashant733@gmail.com>
2022-10-02 12:14:07 +0530

    pattern1

---
+++ lecture 4/Pattern1.cpp
@@ -0,0 +1,26 @@
+// if the number (n) of rows and columns are same 
+// i is the  rows 
+// j is the  columns
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the no. of rows or columns";
+    cin>>n;
+
+    int i=0;
+    while(i<n){ 
+        int j=0;
+        while(j<n){
+            cout<<"*";
+            j++;
+        }
+            cout<<endl;
+        i++;
+    }
+
+    return 0;
+} 
