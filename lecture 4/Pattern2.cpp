2eaf725900335419477fc0e4f36d9f4ab9b7348b
WadekarPrashant <wadekarprashant733@gmail.com>
2022-10-02 12:20:42 +0530

    Pattern2

---
+++ lecture 4/Pattern2.cpp
@@ -0,0 +1,26 @@
+// if the number of rows and columns are not same 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    cout<<"no of rows are - ";
+    cin>>i;
+    cout<<"no of columns are - ";
+    cin>>j;
+     int p=0;
+     while(p<i){
+        int q=0;
+        while(q<j){
+            cout<<"*";
+            q++;
+        }
+        cout<<endl;
+        p++;
+     }
+
+    
+    return 0;
+} 
