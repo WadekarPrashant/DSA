429716f8c20727435d58ebdcf7ee160ae97b0c56
WadekarPrashant <wadekarprashant733@gmail.com>
2022-10-05 19:02:34 +0530

    practice

---
+++ lecture 5/Practice.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int a,b=1;
+    // a = 10 ;
+    // if(++a){
+    //     cout<<b;
+    // }else{
+    //     cout<<++b;
+    // }
+ 
+//    int a=1, b=2;
+//     if(a-->0 || ++b>2){
+//         cout<<"inside if"<<endl;
+//     }else{
+//         cout<<"inside else"<<endl;
+//     }
+//     cout<<a<<" "<<b;
+
+// for(int i=0; i<=5;i--){
+//     cout<<i<<" ";
+//     i++;
+// }
+
+// for(int i=0; i<=15;i+=2){
+//     cout<<i<<" ";
+//     if(i&1){
+//         continue;
+//     }
+//     i++;
+// }
+
+// for(int i=0; i<5;i++){
+//     for(int j=i; j<=5;j++){
+//         cout<<i<<" "<< j <<endl;
+//     }
+// }
+
+// 0 0
+// 0 1
+// 0 2
+// 0 3
+// 0 4
+// 0 5
+// 1 1
+// 1 2
+// .
+// .
+// 1 5
+// 2 2
+// .
+// .
+// .
+// .
+// .
+// 4 5
+
+
+// int n=234;
+// int a= n/100;
+// int o= n/10;
+// int b= o%10;
+// int c= n%10;
+
+// cout <<a<<" "<<b<<" "<<c<<endl;
+
+// int product= a*b*c;
+// int sum=a+b+c;
+// int result= product - sum; 
+// cout<<product<<endl;
+// cout<<sum<<endl;
+// cout<<result<<endl;
+
+  
+    return 0;
+} 
